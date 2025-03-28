62f9abd5d5145331287401d75a226a5e5682a94d
reuel.rai <reuelrai2001@gmail.com>
2025-03-28 18:26:18 +0545

    minor changes

--- script.js
+++ script.js
@@ -31,3 +31,24 @@ function toggleTheme() {
         moonIcon.style.display = 'none';
     }
 }
+
+document.addEventListener("scroll", () => {
+    let sections = document.querySelectorAll("section");
+    let navLinks = document.querySelectorAll("nav ul li a");
+
+    sections.forEach((section) => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 100;
+        let height = section.offsetHeight;
+        let id = section.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((link) => {
+                link.classList.remove("active");
+                if (link.getAttribute("href") === `#${id}`) {
+                    link.classList.add("active");
+                }
+            });
+        }
+    });
+});
